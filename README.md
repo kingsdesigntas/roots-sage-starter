d86f83cbbbef58df93559d06d3c77c1676606bc1
Seb Toombs <sebtoombs@gmail.com>
2020-06-24 09:45:18 +1000

    Initial

---
+++ README.md
@@ -0,0 +1,9 @@
+# Sage 9 Starter
+
+With Tailwind (v1) & Babel
+
+Stylelint removed.
+
+Navwalker added
+
+ESLint tweaks
---
+++ resources/assets/styles/tailwind.config.js
@@ -0,0 +1,18 @@
+const colors = {
+  //Add your theme here
+};
+
+module.exports = {
+  //important: true,
+  //corePlugins: ["container"],
+  theme: {
+    container: {
+      center: true,
+    },
+    extend: {
+      //colors,
+    },
+  },
+  variants: {},
+  plugins: [],
+};
