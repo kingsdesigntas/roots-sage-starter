89e8a6746c4f5cb238672357719f5872bb7a35cb
Seb <sebtoombs@gmail.com>
2021-01-05 16:00:38 +1100

    Update header.blade.php

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,10 +1,12 @@
-<header class="banner">
+<header class="site-header">
   <div class="container">
-    <a class="brand" href="{{ home_url('/') }}">{{ get_bloginfo('name', 'display') }}</a>
-    <nav class="nav-primary">
-      @if (has_nav_menu('primary_navigation'))
+    <div class="flex items-center">
+      {!! get_custom_logo( )!!}
+      <nav class="nav-primary">
+        @if (has_nav_menu('primary_navigation'))
         {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']) !!}
-      @endif
-    </nav>
+        @endif
+      </nav>
+    </div>
   </div>
 </header>
