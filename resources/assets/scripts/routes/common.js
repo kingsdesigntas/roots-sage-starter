6cd009fc5f7223c78a7ef13b4eea6295bcc83534
Seb <sebtoombs@gmail.com>
2021-01-05 15:59:57 +1100

    Update common.js
    add mobile nav alpinejs

--- resources/assets/scripts/routes/common.js
+++ resources/assets/scripts/routes/common.js
@@ -1,3 +1,30 @@
+import "@ryangjchandler/spruce";
+import "alpinejs";
+import focusLock from "dom-focus-lock";
+
+window.focusLock = focusLock;
+
+window.Spruce.store("navigation", {
+  open: false,
+  triggerEl: null,
+});
+
+window.Spruce.watch("navigation.open", (value) => {
+  const store = window.Spruce.store("navigation");
+  const el = document.querySelector("#mobile-navigation");
+  if (value) {
+    focusLock.on(el);
+  } else {
+    focusLock.off(el);
+    if (store.triggerEl) {
+      store.triggerEl.focus();
+      setTimeout(() => {
+        store.triggerEl = null;
+      });
+    }
+  }
+});
+
 export default {
   init() {
     // JavaScript to be fired on all pages
