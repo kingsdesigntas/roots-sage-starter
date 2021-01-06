d00734493a6b9003953f7416a34c8bda22967914
Seb <sebtoombs@gmail.com>
2021-01-06 14:10:58 +1100

    Update header.blade.php

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -4,7 +4,7 @@
       {!! get_custom_logo( )!!}
       <nav class="nav-primary">
         @if (has_nav_menu('primary_navigation'))
-        {!! {{ App\display_menu('primary_navigation') }}!!}
+        {!! App\display_menu('primary_navigation') !!}
         @endif
       </nav>
     </div>
