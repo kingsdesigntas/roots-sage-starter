7c8eb379299b6ec7dd0f7e4b7bc15e80df196d17
Kingsdesign <rik@kingsdesign.net>
2021-06-17 11:07:29 +1000

    hamburger menu icon added for mobile devices

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -7,6 +7,16 @@
         {!! App\display_menu('primary_navigation') !!}
         @endif
       </nav>
+      <button aria-label="open mobile menu" class="block md:hidden ml-2" x-data
+      @click="$store.navigation.triggerEl = $event.currentTarget; $store.navigation.open = !$store.navigation.open;">
+      <svg class="fill-current text-black dark:text-white transition duration-500" width="25" viewBox="0 0 50 50">
+        <g>
+          <rect width="50" height="10" />
+          <rect y="20" width="50" height="10" />
+          <rect y="40" width="50" height="10" />
+        </g>
+      </svg>
+    </button>
     </div>
   </div>
 </header>
